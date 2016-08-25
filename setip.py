19b23a5eead67e671c8c9525f28b24b4a1c015e4
Zeeshan Khan <zkhan1093@gmail.com>
2016-08-25 14:40:22 +0530

    checking current internet connection and asking before quit

--- setip.py
+++ setip.py
@@ -47,7 +47,7 @@ def switchFrame():
         return False
     driver.switch_to_frame(frame)
 
-def setIpMac(ip,mac):
+def navigateToInternetSetup():
     global driver
     for element in driver.find_elements_by_tag_name('a'):
         if element.get_attribute('innerHTML')=='Setup':
@@ -58,6 +58,8 @@ def setIpMac(ip,mac):
             element.send_keys(Keys.RETURN)
             break;
 
+def setIpMac(ip,mac):
+    global driver
     for element in driver.find_elements_by_tag_name('input'):
         if element.get_attribute('name')=='mac_clone' and element.get_attribute('value')=='2':
             element.click()
@@ -73,6 +75,9 @@ def setIpMac(ip,mac):
             ele_mac=element
         if ele_ip and ele_mac:
             break;
+    if ele_ip.get_attribute("value")==ip:
+        print 'already using this combination'
+        return False
     ele_ip.clear()
     ele_ip.send_keys(ip)
     ele_mac.clear()
@@ -86,21 +91,17 @@ def setIpMac(ip,mac):
 
 def isConnectedToDesiredNetwork():
     import subprocess
-    res=CONNECTION_NAME in subprocess.check_output("netsh wlan show interfaces")
-    print 'connected to network',res
-    return res
+    return CONNECTION_NAME in subprocess.check_output("netsh wlan show interfaces")
 
 def isInternetConnected():
     import urllib2
     try:
         response=urllib2.urlopen(URL_TO_CHECK_INTERNET,timeout=1)
-        print 'responce code',response.code
         if response.code in (300, 301, 302, 303, 307):
-            print 'redirected'
             return False
         return True
     except Exception as e:
-        print str(e)
+        print '\b'+str(e)
         return False
 
 file=open(IP_MAC_FILE_LIST_RELATIVE_PATH)
@@ -110,27 +111,50 @@ ip=None
 mac=None
 
 initialized=False
+isLoggedIn=False
+skip=False
+progress=['|','/','-','\\']
+pi=0
 for x in ls:
-    if not isInternetConnected():
+    if skip or not isInternetConnected():
+        skip=False #skipped now need to reset skip counter
         if not initialized:
             init()
         if x:
-            login('Admin','')
-            switchFrame()
-            driver.implicitly_wait(10);
+            if not isLoggedIn:
+                login('Admin','')
+                switchFrame()
+                driver.implicitly_wait(10);
+                navigateToInternetSetup()
             ip,mac=x.split()
             mac=mac[0:2]+':'+mac[2:4]+':'+mac[4:6]+':'+mac[6:8]+':'+mac[8:10]+':'+mac[10:12]
             print 'checking',ip,mac
             if not setIpMac(ip,mac):
                 print 'cannot set ip mac, try again later'
-                break;
-            print 'waiting for router to reboot'
-            time.sleep(35);
+                isLoggedIn=True
+            else:
+                isLoggedIn=False # logged out login again
+                print 'waiting for router to reboot'
+                for t in range(35):
+                    print '\b'+progress[pi%4],'\b\b',
+                    pi+=1
+                    time.sleep(1);
+            print '\bwaiting for network',CONNECTION_NAME,'to connect'
+
             while not isConnectedToDesiredNetwork():
-                print 'waiting for network',CONNECTION_NAME,'to connect'
-                time.sleep(10);
+                print '\b'+progress[pi%4],'\b\b',
+                pi+=1
+                time.sleep(1);
+
     else:
-        print 'Connected to internet, enjoy!'
-        if driver:
-            driver.close()
-        break;
+        print 'Connected to internet.'
+        print 'let me know if you need to try other set of ip and mac [y/N]:',
+        option=raw_input()
+        if option not in ['y','Y','yes','YES']:
+            print 'enjoy then!'
+            if driver:
+                driver.close()
+            break;
+        else:
+            skip=True
+            print 'okay! trying others'
